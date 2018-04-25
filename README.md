0979f800055f671149cafc3ffdff854c4e56adbd
Sir-Fancy <zmarotta@sirfancy.com>
2018-04-25 11:12:30 -0400

    Added highlife.py

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ This is a simple cellular automata library that can be used as a template. Simpl
 Included experiments:
 * forest.py - A forest-growing simulation inspired by a cellular automaton found in the main menu of the game [SUPERHOT](https://superhotgame.com/).
 * fractal.py - A neat little fractalish thing
-* gameoflife.py - Conway's Game of Life
+* gameoflife.py - Conway's Game of Life (B2/S23)
+* highlife.py - Highlife cellular automaton similar to Conway's. (B36/S23)
 * screensaver.py - A neat, evolving fractalish screensaver. (Use a very large window!)
---
+++ highlife.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+from libCellAut import CellAut
+import numpy as np
+import curses
+import time
+import traceback
+import sys
+
+class Fractal(CellAut):
+    def firstframe(self):
+        #Generate start field, init additional variables, override to change start state
+        args = sys.argv
+        if len(args) > 1:
+            if args[1] == "glider":
+                self.field[20,20].stage("#")
+                self.field[21,21].stage("#")
+                self.field[22,19].stage("#")
+                self.field[22,20].stage("#")
+                self.field[22,21].stage("#")
+            else:
+                raise RuntimeError("Preset not found")
+        else:
+            for row in self.field:
+                for cell in row:
+                    if np.random.random() < 0.1:
+                        cell.stage("#")
+    
+    def tick(self):
+        for row in self.field:
+            for cell in row:
+                if cell.val == "#":
+                    if cell.surrounding("#") < 2 or cell.surrounding("#") > 3:
+                        cell.stage(self.blank)
+                elif cell.val == self.blank:
+                    if cell.surrounding("#") in [3, 6]:
+                        cell.stage("#")
+
+def main():
+    try:
+        f = Fractal(blank = " ", speed = 0.2)
+        f.simulate()
+    except KeyboardInterrupt:
+        f.destroy_curses()
+    except BaseException as e:
+        f.destroy_curses()
+        print(traceback.format_exc())
+        
+if __name__ == '__main__':
+    main()
